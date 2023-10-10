eba9dfcddcd08a11add057f1be3d5696e3ba2ed1
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-10 20:53:46 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,3 +1,21 @@
+/*
+1. Global Variable:- head,tail,ans=0, var
+2. Operation Perform
+3. While (invalid case) to make it valid, update tail
+4. ans update
+5. return ans
+
+
+T.C:- O(n)
+
+Ques:- How to know problem is window sliding?
+Ans:- always talk about subarray & max/min length
+
+
+*/    
+
+
+
 int tail = 0, ans = 0, sum = 0;
 for (int head = 0; head < n; head++)
 {
