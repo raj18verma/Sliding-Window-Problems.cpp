8d315cdd48a85e8cd745aa81bf2f90c6f1d9aab0
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-16 05:48:49 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,9 +1,11 @@
 /*
-1. Global Variable:- head,tail,ans=0, var
-2. Operation Perform
-3. While (invalid case) to make it valid, update tail
-4. ans update
-5. return ans
+1. Declare Global Variable like head,tail,ans, operation_var
+2. Use Some data structure as per question requirement (map,vector,etc..)
+3. Put for loop using head variable to sum whole window
+4. Perform operation (asked in question)
+5. If ans become invalid then go inside while loop to make it valid.
+6. Inside while loop exclude the values using tail variable and update tail variable
+7. Update ans and finally return ans
 
 
 T.C:- O(n)
