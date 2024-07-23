f5e68d1d24a0d075f6069c1b89e87674710122b0
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2024-07-23 12:24:27 +0530

    Update 1. Longest Substring without repeating characters.cpp

--- 1. Longest Substring without repeating characters.cpp
+++ 1. Longest Substring without repeating characters.cpp
@@ -3,18 +3,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int length=0 , maxlength=0,j=0;
-        map<char ,int> mp;                     //To count frequency of each word
-        for(int i=0 ;i<s.size(); i++){
-            mp[s[i]]++;
-            length++;
-                while(mp[s[i]]>1){
-                    mp[s[j++]]--;
-                    length--;
-                }
-            maxlength = max(maxlength,length);     //For handling the case when all the characters are different
+        map<char ,int> mp; 
+        int tail=0, head=0, ans=0;
+        for(head=0 ;head<s.size(); head++){
+            while(mp[s[head]]>0){
+                ans=max(ans,head-tail);
+                mp[s[tail]]--;
+                tail++;
+            }
+            mp[s[head]]++;
         }
-        return maxlength;
+        return ans=max(ans,head-tail);
     }
 };
 
